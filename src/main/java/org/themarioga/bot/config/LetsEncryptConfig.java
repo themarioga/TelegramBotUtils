fa4aef5c05e7b1c003eea22ca8ad97814c0c9f89
MARIOGONZALEZALVARAD <marioga2512@gmail.com>
2024-05-02 00:14:16 +0200

    Version 2.0.0 - Be able to disable let's encrypt

--- src/main/java/org/themarioga/bot/config/LetsEncryptConfig.java
+++ src/main/java/org/themarioga/bot/config/LetsEncryptConfig.java
@@ -1,12 +1,14 @@
 package org.themarioga.bot.config;
 
 import com.github.valb3r.letsencrypthelper.tomcat.TomcatWellKnownLetsEncryptChallengeEndpointConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Profile("pro")
 @Configuration
+@Profile("pro")
+@ConditionalOnProperty(prefix = "lets-encrypt-helper", name="enabled", havingValue = "true")
 @Import(TomcatWellKnownLetsEncryptChallengeEndpointConfig.class)
 public class LetsEncryptConfig {
 
